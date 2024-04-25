5b4a9c74b6a5cec75995b7e92d69ff606d269247
AvishkaUmayanga <avishkas97@gmail.com>
2024-04-25 23:32:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import {router as adminRoutes} from './routes/adminRoutes.js'
 import { router as userRoutes } from "./routes/userRoutes.js"
 const app = express()
 
-app.use(cors({origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({origin: 'https://clothing-store-ecru.vercel.app', credentials: true }))
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())
 
