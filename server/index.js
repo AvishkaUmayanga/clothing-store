bb9a86d9258e0bdf24d223a405565235c3c82088
AvishkaUmayanga <avishkas97@gmail.com>
2024-04-25 23:01:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import {router as adminRoutes} from './routes/adminRoutes.js'
 import { router as userRoutes } from "./routes/userRoutes.js"
 const app = express()
 
-app.use(cors({origin: 'http://localhost:5173', credentials: true, methods: ["POST", "GET"]}))
+app.use(cors({origin: 'http://localhost:5173', credentials: true }))
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())
 
